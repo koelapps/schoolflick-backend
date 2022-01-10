9aaa520c02e6b6a7986f9134b54d9ed177e23e51
MINGW64 <md.asad@koelapps.com>
2022-01-10 13:42:56 +0530

    first commit

---
+++ src/main/java/com/koelapps/schoolflickbackend/Student/StudentEntity.java
@@ -0,0 +1,105 @@
+package com.koelapps.schoolflickbackend.Student;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class StudentEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer studentId;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Column(name = "email", nullable = false)
+	private String emailId;
+
+	@Column(name = "gender", nullable = false)
+	private String gender;
+
+	@Column(name = "user_type", nullable = false)
+	private String userType;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	public StudentEntity() {
+
+	}
+
+	public StudentEntity(String firstName, String lastName, String emailId, String gender, String userType, String password) {
+		this.firstName = firstName;
+		this.lastName= lastName;
+		this.emailId = emailId;
+		this.gender = gender;
+		this.userType = userType;
+		this.password = password;
+	}
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+}
---
+++ src/test/java/com/koelapps/schoolflickbackend/SchoolflickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koelapps.schoolflickbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolflickBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
